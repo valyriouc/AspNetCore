94b1463adf90a9df7e916e6dde9438800af840c8
valcor <valyrious.informatics@gmail.com>
2024-12-15 00:38:28 +0100

    some other playing

---
+++ DevToClone/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevToClone.Controllers;
+
+[ApiController]
+public class TestController : ControllerBase
+{
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello world");
+    }
+    
+}
--- DevToClone/Program.cs
+++ DevToClone/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -12,6 +13,5 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
+app.MapDefaultControllerRoute();
 app.Run();
